ecee1414e7e0451b1b5004aecd0e38471df084a0
Michal Porzuc <porzucmichal@gmail.com>
2026-03-31 15:03:46 +0200

    Added demonstraction of calculation functions

--- Application_cw1/Application_cw1/Program.cs
+++ Application_cw1/Application_cw1/Program.cs
@@ -10,5 +10,11 @@ static int multiply(int x, int y)
 
 static double divide(int x, int y)
 {
-    return x / y;
+    return (double)x / y;
 }
+
+Console.WriteLine("Simple Calculator");
+
+Console.WriteLine("5 + 6 = " + add(5,6));
+Console.WriteLine("7 * 6 = " + multiply(7,6));
+Console.WriteLine("8 / 6 = " + divide(8,6));
