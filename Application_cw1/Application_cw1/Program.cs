6f181b58f0f94a63f3eafd1698da07515f580647
Michal Porzuc <porzucmichal@gmail.com>
2026-03-31 15:39:25 +0200

    Change output in main before conflict

--- Application_cw1/Application_cw1/Program.cs
+++ Application_cw1/Application_cw1/Program.cs
@@ -15,6 +15,8 @@ static double divide(int x, int y)
 
 Console.WriteLine("Simple Calculator - doszlo do zmianygit");
 
+Console.WriteLine("Conflict version B");
+
 Console.WriteLine("5 + 6 = " + add(5,6));
 Console.WriteLine("7 * 6 = " + multiply(7,6));
 Console.WriteLine("8 / 6 = " + divide(8,6));
