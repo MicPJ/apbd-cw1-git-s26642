49f3cf2b97738bbaf8a29034a0aac6bcdfd91eb7
Michal Porzuc <porzucmichal@gmail.com>
2026-03-31 15:17:55 +0200

    Main branch changed before merging feature-max

--- Application_cw1/Application_cw1/Program.cs
+++ Application_cw1/Application_cw1/Program.cs
@@ -13,7 +13,7 @@ static double divide(int x, int y)
     return (double)x / y;
 }
 
-Console.WriteLine("Simple Calculator");
+Console.WriteLine("Simple Calculator - main branch change before merge");
 
 Console.WriteLine("5 + 6 = " + add(5,6));
 Console.WriteLine("7 * 6 = " + multiply(7,6));
