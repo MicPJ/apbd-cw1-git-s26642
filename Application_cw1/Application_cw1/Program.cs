aae6a762f9cefbc38d9d4fda77e423c90a5a1200
Michal Porzuc <porzucmichal@gmail.com>
2026-03-31 15:10:36 +0200

    Added average method

--- Application_cw1/Application_cw1/Program.cs
+++ Application_cw1/Application_cw1/Program.cs
@@ -18,3 +18,7 @@ Console.WriteLine("Simple Calculator");
 Console.WriteLine("5 + 6 = " + add(5,6));
 Console.WriteLine("7 * 6 = " + multiply(7,6));
 Console.WriteLine("8 / 6 = " + divide(8,6));
+
+static double average(int[] tab){
+    return tab.Average();
+}
